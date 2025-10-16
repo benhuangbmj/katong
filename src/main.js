a1635751384a69328dd5ddfb2d7bfd1a72f1740d
Ben Huang <benjaminwong1985@gmail.com>
2025-10-16 14:37:36 -0400

    increment: build an agent that pathfinds

--- src/main.js
+++ src/main.js
@@ -1,11 +1,55 @@
 import kaplay from "kaplay";
-// import "kaplay/global"; // uncomment if you want to use without the k. prefix
+//import "kaplay/global"; // uncomment if you want to use without the k. prefix
 
-const k = kaplay();
+kaplay();
 
-k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
+loadRoot("./"); // A good idea for Itch.io publishing later
+loadSprite("bean", "sprites/bean.png");
+loadSprite("coin", "sprites/coin.png");
+loadSprite("spike", "sprites/spike.png");
+loadSprite("steel", "sprites/steel.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+const myLevel = level(
+  [
+    "===========",
+    "=         =",
+    "=         =",
+    "=====     =",
+    "=   =     =",
+    "=   =  =  =",
+    "=      =  =",
+    "=      ====",
+    "=         =",
+    "===========",
+  ],
+  {
+    tileWidth: 64,
+    tileHeight: 64,
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+    tiles: {
+      "=": () => [
+        sprite("steel"),
+        area(),
+        body({ isStatic: true }),
+        tile({ isObstacle: true }),
+      ],
+      $: () => [sprite("coin"), area(), pos(0, -9)],
+      "^": () => [sprite("spike"), area(), "danger"],
+    },
+  }
+);
+add([myLevel]);
+const bean = myLevel.spawn(
+  [
+    sprite("bean"),
+    anchor("center"),
+    pos(64, 64),
+    tile(),
+    agent({ agentSpeed: 640, allowDiagonals: true }),
+    "bean",
+  ],
+  vec2(1, 1)
+);
+onClick(() => {
+  bean.setTarget(mousePos());
+});
