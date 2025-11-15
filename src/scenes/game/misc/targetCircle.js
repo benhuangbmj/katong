e9dd3926429d4c04dea66eb39545fa9f6a8b61f3
Ben Huang <benjaminwong1985@gmail.com>
2025-11-15 14:29:10 -0500

    refactor: decouple the target circle object

---
+++ src/scenes/game/misc/targetCircle.js
@@ -0,0 +1,30 @@
+let targetCircle;
+
+function destroy() {
+  if (targetCircle) {
+    targetCircle.destroy();
+  }
+}
+
+function get() {
+  return targetCircle;
+}
+
+function set(level, position) {
+  const [TILE_WIDTH, TILE_HEIGHT] = [level.tileWidth(), level.tileHeight()];
+  targetCircle = level.spawn(
+    [
+      circle(24, { fill: false }),
+      pos(TILE_WIDTH / 2, TILE_HEIGHT / 2),
+      outline(4, Color.GREEN),
+      animate(),
+      z(-1),
+    ],
+    level.pos2Tile(position)
+  );
+  targetCircle.animate("radius", [24, 16, 24], {
+    duration: 0.5,
+  });
+}
+
+export default { destroy, set, get };
