fe1ffa32f0cfdcdee51f5ee5ff824047adbfe08b
Ben Huang <benjaminwong1985@gmail.com>
2025-11-15 14:30:16 -0500

    refactor: include the chase and adjust function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-function snapToTileCenter({ level, pos }) {
-  const tilePos = level.pos2Tile(pos);
+function snapToTileCenter({ level, position }) {
+  const tilePos = level.pos2Tile(position);
   const tileWorldPos = level.tile2Pos(tilePos);
   const output = vec2(
     tileWorldPos.x + 0.5 * level.tileWidth(),
@@ -7,5 +7,24 @@ function snapToTileCenter({ level, pos }) {
   );
   return output;
 }
+function chase(subject, target) {
+  return subject.onUpdate(() => {
+    subject.setTarget(target.pos);
+  });
+}
+function adjust(obj, cb = () => {}) {
+  const currentTile = obj.tilePos;
+  const currentPos = myLevel.tile2Pos(currentTile);
+  const targetPos = [
+    currentPos.x + TILE_WIDTH / 2,
+    currentPos.y + TILE_HEIGHT / 2,
+  ];
+  obj.unuse("body");
+  obj.setTarget(vec2(...targetPos));
+  obj.onTargetReached(() => {
+    obj.use(body());
+    cb();
+  });
+}
 
-export default { snapToTileCenter };
+export default { snapToTileCenter, chase, adjust };
